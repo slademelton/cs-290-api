898b9cbe0ebb75587650202a89d6d5e786646f20
slademelton <slademelton00@gmail.com>
2021-10-26 13:47:10 -0400

    add get single player and create player logic

--- controllers/players.js
+++ controllers/players.js
@@ -1,4 +1,5 @@
 const Player = require('../models/player');
+const Team = require('../models/team');
 const asyncHandler = require('../middleware/asyncHandler');
 const ErrorHandler = require('../utils/errorResponse');
 
@@ -22,5 +23,46 @@ exports.getPlayers = asyncHandler(async (req, res, next) => {
         success: true,
         count: players.length,
         data: players,
+    });
+});
+
+exports.getPlayer = asyncHandler(async (req, res, next) => {
+   const player = await Player.findById(req.params.playerId).populate({
+       path: 'team',
+       select: 'name description'
+   })
+
+   if (!player) {
+       return next(new ErrorHandler(`No player with ID of ${req.params.playerId}`, 404));
+   }
+
+
+
+    res.status(200).json({
+        success: true,
+        data: player,
+    });
+});
+
+// POST /api/v1/teams/:id/players
+exports.createPlayer = asyncHandler(async (req, res, next) => {
+    //update req.body to add team _id
+    req.body.team = req.params.id;
+
+
+    //get the team
+    const team = await Team.findById(req.params.id);
+
+    //make sure team exists
+    if (!team) {
+        return next (new ErrorHandler(`No team with ID of ${req.params.id}`, 404));
+    }
+    //create player, assigning it to team
+    const player = await Player.create(req.body); //will include the team _id we added
+
+    //send response
+    res.status(201).json({
+        success: true,
+        data: player
     })
-})
+});
--- routes/players.js
+++ routes/players.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 
 const { 
-    getPlayers
+    getPlayers,
+    getPlayer,
+    createPlayer
 } = require('../controllers/players');
 
-router.route('/').get(getPlayers);
+// /api/v1/players
+// /api/v1/teams/:id/players
+router.route('/').get(getPlayers).post(createPlayer);
+router.route('/:playerId').get(getPlayer);
 
 module.exports = router;
