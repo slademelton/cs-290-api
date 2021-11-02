db4f58c3146521192ab24e1f630d9bd02c05f3fb
slademelton <slademelton00@gmail.com>
2021-11-02 16:53:53 -0400

    Add JWT functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,10 +12,14 @@ exports.registerUser = asyncHandler(async (req, res, next) => {
         password,
         role
     })
+    
+    //create JWT Token
+    const token = user.getSignedJwtToken()
 
     res.status(201).json({
         success: true,
         data: user.username,
+        token,
     });
 
 });
--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = mongoose.Schema({
     username: {
@@ -47,5 +48,15 @@ UserSchema.pre('save', async function(next) {
     this.password = await bcrypt.hash(this.password, salt);
 
 });
+
+//============
+//method
+//============
+//sign JWT and return
+UserSchema.methods.getSignedJwtToken = function() {
+    return jwt.sign({id: this._id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES
+    });
+};
 
 module.exports = mongoose.model('User', UserSchema);
