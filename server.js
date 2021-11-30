bec922076dfe0fe228bef8bcdc57f3df7be69533
slademelton <slademelton00@gmail.com>
2021-11-29 20:30:24 -0500

    Add rate limiting, HPP prevention, and CORS

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xssClean = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 
 //Import Local Files
 const connectToDb = require('./config/connectToDb');
@@ -21,6 +24,9 @@ dotenv.config({path: "./config/config.env"});
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
+
+//Allow CORS
+app.use(cors());
 
 //Cookie Parser
 app.use(cookieParser());
@@ -39,6 +45,17 @@ app.use(helmet());
 
 //prevent xss attacks
 app.use(xssClean());
+
+//rate limiter
+const limiter = rateLimit({
+    windowMs: process.env.RATE_LIMIT_MINUTES * 60 * 1000,
+    max: process.env.RATE_LIMIT_REQUESTS,
+    message: `Too many requests. You are only allowed ${process.env.RATE_LIMIT_REQUESTS} request per ${process.env.RATE_LIMIT_MINUTES} minutes`
+})
+app.use(limiter);
+
+//prevent http param pollution
+app.use(hpp());
 
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
